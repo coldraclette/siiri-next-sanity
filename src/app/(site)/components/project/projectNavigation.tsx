33649512ee5ede3f6fdff93ec9415ed256441dd3
Timo Tännler <timo.taennler@protonmail.com>
2024-01-13 18:58:22 +0200

    Refactor project navigation component

--- src/app/(site)/components/project/projectNavigation.tsx
+++ src/app/(site)/components/project/projectNavigation.tsx
@@ -5,26 +5,32 @@ import ArrowRight from '../../../../../public/icons/arrow-right.svg';
 import { PreviousProject } from '../../types';
 
 interface ProjectNavigationProps {
-  prev: PreviousProject;
-  next: PreviousProject;
+  prev: PreviousProject | null;
+  next: PreviousProject | null;
 }
 
 export default function ProjectNavigation({
   prev,
   next,
 }: ProjectNavigationProps) {
+
+  if (!prev && !next) return null;
   return (
     <>
-      <div className="fixed left-3 top-1/2 h-10 w-10 -translate-y-1/2 md:h-20 md:w-20">
-        <Link href={`/${prev.slug.current}`}>
-          <ArrowLeft />
-        </Link>
-      </div>
-      <div className="fixed right-3 top-1/2 h-10 w-10 -translate-y-1/2 md:h-20 md:w-20">
-        <Link href={`/${next.slug.current}`}>
-          <ArrowRight />
-        </Link>
-      </div>
+      {prev && (
+        <div className="fixed left-3 top-1/2 h-10 w-10 -translate-y-1/2 md:h-20 md:w-20">
+          <Link href={`/${prev.slug.current}`}>
+            <ArrowLeft />
+          </Link>
+        </div>
+      )}
+      {next && (
+        <div className="fixed right-3 top-1/2 h-10 w-10 -translate-y-1/2 md:h-20 md:w-20">
+          <Link href={`/${next.slug.current}`}>
+            <ArrowRight />
+          </Link>
+        </div>
+      )}
     </>
   );
 }
